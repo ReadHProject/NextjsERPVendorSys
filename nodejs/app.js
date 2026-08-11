3c47df12b4469ba348dafbc7f29aba24eaa4f33e
ReadHProject <readheadproject@gmail.com>
2026-08-11 22:47:05 +0530

    fix: add nodejs subfolder entry proxies for Hostinger path resolution

---
+++ nodejs/app.js
@@ -0,0 +1,2 @@
+// Entry point proxy for Hostinger LiteSpeed subfolder resolution (.builds/current/nodejs/app.js)
+require("../server.js");
---
+++ nodejs/index.js
@@ -0,0 +1,2 @@
+// Entry point proxy for Hostinger LiteSpeed subfolder resolution (.builds/current/nodejs/index.js)
+require("../server.js");
