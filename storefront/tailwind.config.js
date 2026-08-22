a2764a2008eeb7e698b891174bbc8b94e26b28c6
ReadHProject <readheadproject@gmail.com>
2026-08-23 00:58:31 +0530

    feat: initialize tailwind configuration files for admin and storefront workspaces

--- storefront/tailwind.config.js
+++ storefront/tailwind.config.js
@@ -1,9 +1,14 @@
+const path = require("path");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
   content: [
-    "./app/**/*.{js,jsx}",
-    "./components/**/*.{js,jsx}",
+    path.join(__dirname, "./app/**/*.{js,jsx,ts,tsx}"),
+    path.join(__dirname, "./components/**/*.{js,jsx,ts,tsx}"),
+    path.join(__dirname, "./lib/**/*.{js,jsx,ts,tsx}"),
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
